df23a9bff3202e1d0cd9947e49865f304aca98bf
Victor Savkin <vic.savkin@gmail.com>
2016-02-29 21:48:05 +0000

    87221b7 fix(transformers): record reflection info about abstract classes

--- bundles/router.dev.js
+++ bundles/router.dev.js
@@ -1143,7 +1143,9 @@ System.register("angular2/src/router/hash_location_strategy", ["angular2/core", 
     };
     HashLocationStrategy.prototype.path = function() {
       var path = this._platformLocation.hash;
-      return (path.length > 0 ? path.substring(1) : path) + location_strategy_1.normalizeQueryParams(this._platformLocation.search);
+      if (!lang_1.isPresent(path))
+        path = '#';
+      return (path.length > 0 ? path.substring(1) : path);
     };
     HashLocationStrategy.prototype.prepareExternalUrl = function(internal) {
       var url = location_strategy_1.joinWithSlash(this._baseHref, internal);
