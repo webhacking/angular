7bbb9847d3a9a221c2ae2768b8f06eccead4c8fb
vsavkin <avix1000@gmail.com>
2016-02-29 18:48:49 +0000

    331b9c1 fix(transformers): special case types some built-in types, so they can be resolved

--- es6/prod/src/router/hash_location_strategy.js
+++ es6/prod/src/router/hash_location_strategy.js
@@ -70,12 +70,11 @@ export let HashLocationStrategy = class extends LocationStrategy {
         // the hash value is always prefixed with a `#`
         // and if it is empty then it will stay empty
         var path = this._platformLocation.hash;
-        if (!isPresent(path))
-            path = '#';
         // Dart will complain if a call to substring is
         // executed with a position value that extends the
         // length of string.
-        return (path.length > 0 ? path.substring(1) : path);
+        return (path.length > 0 ? path.substring(1) : path) +
+            normalizeQueryParams(this._platformLocation.search);
     }
     prepareExternalUrl(internal) {
         var url = joinWithSlash(this._baseHref, internal);
